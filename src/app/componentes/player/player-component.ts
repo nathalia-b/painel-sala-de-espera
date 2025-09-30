0627d2965390cb6d30d0aafc4dca693fa5bc177a
Nathalia Bezerra <nathalyabezerra@live.com>
2025-09-30 06:00:17 -0300

    feat: Adiciona autoplay+loop

--- src/app/componentes/player/player-component.ts
+++ src/app/componentes/player/player-component.ts
@@ -8,10 +8,17 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
   styleUrl: './player-component.scss',
 })
 export class PlayerComponent {
+  private readonly videoId = 'WdxYgjjPSjg';
   urlSegura: SafeResourceUrl;
 
   constructor(private sanitizer: DomSanitizer) {
-    const url = 'https://www.youtube.com/embed/WdxYgjjPSjg?si=BwBlusq2xbmWHi_s';
+    const url =
+      `https://www.youtube.com/embed/${this.videoId}` +
+      `?autoplay=1` +
+      `&mute=1` +
+      `&loop=1` +
+      `&playlist=${this.videoId}`;
+
     this.urlSegura = this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 }
