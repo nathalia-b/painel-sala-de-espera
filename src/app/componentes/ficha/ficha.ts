9144cb3cf24ce4621dac22e86c2e6263a8bc2471
Nathalia Bezerra <nathalyabezerra@live.com>
2025-10-02 19:19:46 -0300

    feat: Adiciona especialidade à ficha do histórico & formataçao do nome (Jane Doe -> J. Doe)

--- src/app/componentes/ficha/ficha.ts
+++ src/app/componentes/ficha/ficha.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 export interface Dados {
   senha: string;
   nome: string;
+  especialidade: string;
   hora: Date;
   guiche: string;
 }
@@ -15,6 +16,23 @@ export interface Dados {
   styleUrl: './ficha.scss',
 })
 export class Ficha {
+  //  Recebe se a chamada é a atual
   @Input() chamadaAtual: boolean = false;
+
+  //  Recebe os dados da chamada feita
   @Input() dadosSenha: Dados | undefined;
+
+  get nomeFormatado(): string {
+    if (!this.dadosSenha?.nome) return '';
+
+    //  Separa nome e sobrenome
+    const nomeCompleto: string[] = this.dadosSenha.nome.trim().split(' ');
+    const nome = nomeCompleto[0];
+    const sobrenome = nomeCompleto[1];
+
+    if (nome && !sobrenome) return nome; // Caso só haja um nome, retorna ele completo.
+
+    //  Retorna a primeira letra do primeiro nome e o sobrenome
+    return `${nome[0].toUpperCase()}. ${sobrenome}`;
+  }
 }
